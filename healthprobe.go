796abe7ccbafe9fa1347b10d9e7dd15c51539e6c
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2017-04-30 16:25:56 +0200

    mariadb: Start to use the context of the http request
    Use the context of the http request and add a timeout to it. The most
    simple test of nc -l 3306 and then connecting to it... doesn't abort
    the execution.

--- healthprobe.go
+++ healthprobe.go
@@ -10,10 +10,11 @@ import _ "github.com/go-sql-driver/mysql"
 import "net/http"
 import "time"
 import "log"
+import "context"
 
 // Connect to MariaDB Galera and check if this specific node is
 // considered ready and accepting writes
-func checkMysql(user string, password string, host string, port int) (int, string) {
+func checkMysql(ctx context.Context, user string, password string, host string, port int) (int, string) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/", user, password, host, port)
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
@@ -22,7 +23,7 @@ func checkMysql(user string, password string, host string, port int) (int, strin
 	}
 
 	// When the node returns a value of ON it can accept write-sets from the cluster
-	rows, err := db.Query("SHOW GLOBAL STATUS LIKE 'wsrep_ready'")
+	rows, err := db.QueryContext(ctx, "SHOW GLOBAL STATUS LIKE 'wsrep_ready'")
 	if err != nil {
 		db.Close()
 		fmt.Print(err, "\n")
@@ -54,7 +55,10 @@ func main() {
 
 	// This drives the health check..
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		code, reason := checkMysql("user", "pass", "localhost", 3306)
+		ctx := req.Context()
+		ctx, cancel := context.WithTimeout(ctx, 1 * time.Second)
+		code, reason := checkMysql(ctx, "user", "pass", "localhost", 3306)
+		cancel()
 		w.WriteHeader(code)
 		w.Write([]byte(reason))
 	})
